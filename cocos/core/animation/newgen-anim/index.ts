972f7570e63fae4e526c4b985e5783efd3c00f31
shrinktofit <leslieleigh@outlook.com>
2021-06-07 14:22:06 +0800

    Update

--- cocos/core/animation/newgen-anim/index.ts
+++ cocos/core/animation/newgen-anim/index.ts
@@ -1,6 +1,6 @@
 export { InvalidTransitionError, VariableNotDefinedError } from './errors';
-export { PoseGraph } from './pose-graph';
-export type { Transition, Layer, PoseSubgraph, GraphNode, Variable, VariableType, LayerBlending } from './pose-graph';
+export { PoseGraph, VariableType, LayerBlending } from './pose-graph';
+export type { Transition, Layer, PoseSubgraph, GraphNode, Variable } from './pose-graph';
 export { Condition } from './condition';
 export type { Value } from './variable';
 export { PoseNode } from './pose-node';
