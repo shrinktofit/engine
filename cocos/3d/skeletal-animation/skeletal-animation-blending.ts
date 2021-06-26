d84a0f0cc779e86327346126135166d239d59ac4
Leslie Leigh <leslieleigh@outlook.com>
2021-06-26 17:21:19 +0800

    Fix blending with zero weight

--- cocos/3d/skeletal-animation/skeletal-animation-blending.ts
+++ cocos/3d/skeletal-animation/skeletal-animation-blending.ts
@@ -155,6 +155,9 @@ class Vec3PropertyBlendState extends PropertyBlendState<Vec3> {
     }
 
     public blend (value: Readonly<Vec3>, weight: number) {
+        if (weight === 0.0) {
+            return;
+        }
         const { blendedValue } = this;
         if (weight === 1.0) {
             Vec3.copy(blendedValue, value);
@@ -176,6 +179,9 @@ class QuatPropertyBlendState extends PropertyBlendState<Quat> {
     }
 
     public blend (value: Readonly<Quat>, weight: number) {
+        if (weight === 0.0) {
+            return;
+        }
         const { blendedValue, blendedWeight } = this;
         if (weight === 1.0) {
             Quat.copy(blendedValue, value);
