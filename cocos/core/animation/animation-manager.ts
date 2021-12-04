8411ad9e1624715bbc4d7312f7a9d61054247f75
arsen2010 <arsen2010@126.com>
2021-12-04 09:57:07 +0800

    initial support editing animation-mask

--- cocos/core/animation/animation-manager.ts
+++ cocos/core/animation/animation-manager.ts
@@ -49,7 +49,7 @@ interface ISocketData {
 
 @ccclass
 export class AnimationManager extends System {
-    public get blendState () {
+    public get blendState (): LegacyBlendStateBuffer {
         return this._blendStateBuffer;
     }
 
@@ -61,7 +61,7 @@ export class AnimationManager extends System {
         thisArg: any;
         args: any[];
     }[] = [];
-    private _blendStateBuffer: BlendStateBuffer = new LegacyBlendStateBuffer();
+    private _blendStateBuffer: LegacyBlendStateBuffer = new LegacyBlendStateBuffer();
     private _sockets: ISocketData[] = [];
 
     public addCrossFade (crossFade: CrossFade) {
