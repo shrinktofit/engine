acd0e6559e743bda54378803cbb5c61c2d4eb959
arsen2010 <arsen2010@126.com>
2021-12-15 15:58:18 +0800

    support animation-mask editing

--- editor/inspector/assets/animation-mask.js
+++ editor/inspector/assets/animation-mask.js
@@ -1,6 +1,14 @@
 exports.template = `
 <section class="asset-animation-mask">
-dsd
+    <div class="header">
+        <ui-button class="import">
+            <ui-icon value="import"></ui-icon>
+            <ui-label value="i18n:ENGINE.assets.animationMask.importSkeleton"></ui-label>
+        </ui-button>
+    </div>
+    <div class="content">
+        <ui-tree class="tree"></ui-tree>
+    </div>
 </section>
 `;
 
@@ -13,27 +21,44 @@ exports.methods = {
         }
     },
     async query(uuid) {
-        return await Editor.Message.request('scene', 'query-animation-mask', uuid);
+        this.queryData = await Editor.Message.request('scene', 'query-animation-mask', uuid);
+        this.updateTree();
     },
     async apply() {
         this.reset();
-        await Editor.Message.request('scene', 'apply-animation-mask', this.asset.uuid, this.physicsMaterial);
+        await Editor.Message.request('scene', 'apply-animation-mask', this.asset.uuid);
     },
     reset() {
         this.dirtyData.origin = this.dirtyData.realtime;
         this.dirtyData.uuid = '';
     },
     async dataChange() {
-        await Editor.Message.request('scene', 'change-animation-mask', this.physicsMaterial);
+        console.log(this.queryData);
+        await Editor.Message.request('scene', 'change-animation-mask', {
+            method: 'change-dump',
+            dump: this.queryData,
+        });
+
         this.setDirtyData();
         this.dispatch('change');
+    },
+
+    jointEnableChange(key, checked, loop) {
+        const { children, origin } = this.flatData[key];
+        origin.value.enabled.value = checked;
+
+        if (loop) {
+            children.forEach(childKey => {
+                this.jointEnableChange(childKey, checked, loop);
+            });
+        }
     },
 
     /**
      * Detection of data changes only determines the currently selected technique
      */
     setDirtyData() {
-        this.dirtyData.realtime = JSON.stringify(this.physicsMaterial);
+        this.dirtyData.realtime = JSON.stringify(this.queryData);
 
         if (!this.dirtyData.origin) {
             this.dirtyData.origin = this.dirtyData.realtime;
@@ -44,18 +69,176 @@ exports.methods = {
         const isDirty = this.dirtyData.origin !== this.dirtyData.realtime;
         return isDirty;
     },
-};
-exports.$ = {
-    container: '.asset-animation-mask',
+
+    convertData(jointArr) {
+        const collator = new Intl.Collator('en', {
+            numeric: true,
+            sensitivity: 'base',
+        });
+
+        jointArr.sort((a, b) => {
+            return collator.compare(a.value.path.value, b.value.path.value);
+        });
+
+        console.log(jointArr);
+
+        const flatData = {};
+
+        for (const joint of jointArr) {
+            const key = joint.value.path.value;
+            if (!key) {
+                continue;
+            }
+
+            const parentKey = key.substr(0, key.lastIndexOf('/'));
+            if (!flatData[parentKey]) {
+                flatData[parentKey] = { children: [key] };
+            } else {
+                if (!flatData[parentKey].children.includes(key)) {
+                    flatData[parentKey].children.push(key);
+                }
+            }
+
+            if (!flatData[key]) {
+                flatData[key] = { origin: joint, children: [] };
+            } else {
+                flatData[key].origin = joint;
+            }
+        }
+        console.log(flatData);
+
+        const treeData = [];
+        for (const key in flatData) {
+            if (!flatData[key].detail) {
+                treeDataFromFlatData(treeData, flatData[key].children);
+                break;
+            }
+        }
+
+        function treeDataFromFlatData(treeArr, keys) {
+            if (!Array.isArray(keys)) {
+                return;
+            }
+
+            keys.forEach((key) => {
+                const { origin, children } = flatData[key];
+
+                const name = origin.value.path.value;
+                const treeNode = {
+                    detail: {
+                        key,
+                        name: name.substr(name.lastIndexOf('/') + 1),
+                        checked: origin.value.enabled.value,
+                    },
+                    children: [],
+                };
+                treeArr.push(treeNode);
+
+                treeDataFromFlatData(treeNode.children, children);
+            });
+        }
+
+        console.log(treeData);
+
+        return { flatData, treeData };
+    },
+
+    updateTree() {
+        const convertData = this.convertData(this.queryData._jointMasks.value);
+        this.flatData = convertData.flatData;
+        this.$.tree.tree = convertData.treeData;
+    },
 };
 
+exports.$ = {
+    container: '.asset-animation-mask',
+    import: '.import',
+    tree: '.tree',
+};
+
+exports.style = `
+.asset-animation-mask {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+}
+
+.header {
+    margin-top: 10px;
+}
+
+.content {
+    flex: 1;
+    margin-top: 10px;
+}
+
+.content .tree {
+    height: 100%;
+}
+
+.header .import {
+    display: block;
+    text-align: center;
+}
+`;
+
 exports.ready = function() {
+    const panel = this;
+
+    panel.$.import.addEventListener('change', (event) => {
+        const rawTimestamp = Date.now();
+        Editor.Panel._kitControl.open({
+            $kit: event.target,
+            name: 'ui-kit.searcher',
+            timestamp: rawTimestamp,
+            type: 'asset',
+            droppable: 'cc.Skeleton',
+            events: {
+                async confirm(uuid) {
+                    panel.queryData = await Editor.Message.request('scene', 'change-animation-mask', {
+                        method: 'import-skeleton',
+                        uuid,
+                    });
+
+                    panel.updateTree();
+                    panel.setDirtyData();
+                    panel.dispatch('change');
+                },
+            },
+        });
+    });
+
     // Used to determine whether the material has been modified in isDirty()
-    this.dirtyData = {
+    panel.dirtyData = {
         uuid: '',
         origin: '',
         realtime: '',
     };
+
+    panel.$.tree.setTemplate('left', '<ui-checkbox tooltip="i18n:ENGINE.assets.animationMask.nodeEnableTip"></ui-checkbox>');
+    panel.$.tree.setTemplateInit('left', ($left) => {
+        $left.$checkbox = $left.querySelector('ui-checkbox');
+        $left.$checkbox.addEventListener('click', (event) => {
+            const key = $left.data.detail.key;
+            const origin = panel.flatData[key].origin;
+            panel.jointEnableChange(key, !origin.value.enabled.value, event.altKey);
+            panel.dataChange();
+            panel.$.tree.render();
+        });
+    });
+    panel.$.tree.setRender('left', ($left) => {
+        const key = $left.data.detail.key;
+        const origin = panel.flatData[key].origin;
+        $left.$checkbox.value = origin.value.enabled.value;
+    });
+
+    panel.$.tree.setTemplate('text', `<span class="name"></span>`);
+    panel.$.tree.setTemplateInit('text', ($text) => {
+        $text.$name = $text.querySelector('.name');
+    });
+    panel.$.tree.setRender('text', ($text, data) => {
+        $text.$name.innerHTML = data.detail.name;
+    });
 };
 
 exports.update = async function(assetList, metaList) {
@@ -74,26 +257,7 @@ exports.update = async function(assetList, metaList) {
         this.dirtyData.origin = '';
     }
 
-    this.physicsMaterial = await this.query(this.asset.uuid);
-
-    for (const key in this.physicsMaterial) {
-        const dump = this.physicsMaterial[key];
-
-        if (!dump.visible) {
-            continue;
-        }
-
-        // reuse
-        if (!this.$[key]) {
-            this.$[key] = document.createElement('ui-prop');
-            this.$[key].setAttribute('type', 'dump');
-            this.$[key].addEventListener('change-dump', this.dataChange.bind(this));
-        }
-
-        this.$.container.appendChild(this.$[key]);
-        this.updateReadonly(this.$[key]);
-        this.$[key].render(dump);
-    }
+    await this.query(this.asset.uuid);
 
     this.setDirtyData();
 };
