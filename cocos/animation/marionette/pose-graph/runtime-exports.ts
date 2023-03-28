bbc1f676c5bfdb350186cfbf20b1504060c8cdf4
Leslie Leigh <leslieleigh@outlook.com>
2023-03-28 20:18:48 +0800

    XNode examples
    import xnodes

--- cocos/animation/marionette/pose-graph/runtime-exports.ts
+++ cocos/animation/marionette/pose-graph/runtime-exports.ts
@@ -2,5 +2,7 @@ import './pose-nodes/get-all-previous-layers-result';
 import './pose-nodes/use-cached-pose';
 
 import './x-nodes/get-variable';
+import './x-nodes/builtin/math/general';
+import './x-nodes/builtin/math/trigonometry';
 
 export {};
